66dde750760791a40dfb9368fb3e77e0c4c4cb08
Sergey <kashtanov_sv@bk.ru>
2022-06-16 13:09:56 +0300

    Zadanie6

---
+++ src/Zadanie6/Animal.java
@@ -0,0 +1,8 @@
+package Zadanie6;
+
+public class Animal {
+    String run = "";
+    public void infoRun(){
+        System.out.println("Животное побежало:" + run);
+    }
+}
---
+++ src/Zadanie6/main.java
@@ -0,0 +1,11 @@
+package Zadanie6;
+
+public class main {
+    public static void main(String[] args) {
+        Dog dog = new Dog(" 150 m", "10 m");
+        dog.infoRun();
+        dog.infoSwim();
+        Cat cat = new Cat(" 10 m");
+        cat.infoRun();
+    }
+}
